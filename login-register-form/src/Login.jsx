a8f35586c010142ed6730b5c345f23f186e71511
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-16 21:50:54 +0530

    eight

--- login-register-form/src/Login.jsx
+++ login-register-form/src/Login.jsx
@@ -17,7 +17,7 @@ export const Login = (props) => {
                 <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
                 <label htmlFor="password">password</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="********" id="password" name="password" />
-                <button type="submit">Log In</button>
+                <button className="btn"  type="submit">Log In</button>
             </form>
             <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
         </div>
